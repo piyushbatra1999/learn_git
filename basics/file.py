f43c7a6b26f39526f1702a3b1d67b191b1b094f8
Piyush Batra <piyushbatra1999@gmail.com>
2021-03-10 05:37:29 -0800

    demonstrating keeping local changes if you revert a commit"

--- basics/file.py
+++ basics/file.py
@@ -1,2 +1,6 @@
 # basic python file
+# Line to demonstarate soft reset.
+#another line to demonstrate keeping chamges in local file even if you pushed the commit.
+# if not pushed yet git reset HEAD~1 --soft
+# if pushed revert to last commit , push those changes and then revert again with --no-commit flag with hash of current commit.
 
